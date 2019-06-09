f97a3bcf7592b7aae6b88bfafc88a9014e6b309d
Kayla Vincenzetti <knvg14@gmail.com>
2019-06-08 23:55:25 -0600

    added function for userChoice

--- logic.js
+++ logic.js
@@ -1,9 +1,14 @@
+//an array to hold the computer choices 
 let compChoice = ["r", 'p', 's'];
-let userChoice = "";
 
+//chooses random letter from above array for computer choice
 compSelection = compChoice[Math.floor(Math.random() * compChoice.length)];
 console.log(compSelection);
 
+//function to allow user to select letter and log to console
+document.onkeyup = function(event) {
 
+let userChoice = event.key; 
+console.log(userChoice);
 
-
+}
