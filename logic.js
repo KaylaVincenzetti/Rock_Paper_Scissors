b51f505686d91c6dd8da487a664dfe0c03baf1cf
Kayla Vincenzetti <knvg14@gmail.com>
2019-06-09 21:38:25 -0600

    added dynamic html elements

--- logic.js
+++ logic.js
@@ -3,12 +3,16 @@ let compChoice = ["r", 'p', 's'];
 
 //chooses random letter from above array for computer choice
 compSelection = compChoice[Math.floor(Math.random() * compChoice.length)];
-console.log(compSelection);
+    compChoice = document.getElementById("comp");
+    compChoice.innerHTML = "Computer Choice: " + compSelection;
+    console.log(compSelection);
 
 //function to allow user to select letter and log to console
 document.onkeyup = function(event) {
+    let userChoice = event.key; 
+    userChoice = document.getElementById("user");
+    userChoice.innerHTML = "User Choice: " + event.key;
+    console.log(userChoice);
 
-let userChoice = event.key; 
-console.log(userChoice);
 
 }
